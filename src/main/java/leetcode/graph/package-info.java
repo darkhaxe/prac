6fe70c7bdc1ff56a1e95234bb2d23ce179c9c795
haxe <792272581@qq.com>
2020-02-04 14:06:45 +0800

    完全平方数

---
+++ src/main/java/leetcode/graph/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * 相关题目
+ * 279.perfect squares
+ * 127.word ladder
+ * 126.word ladder II
+ * @author haze
+ * @date created at 2020/2/3 8:40 下午
+ */
+package leetcode.graph;
---
+++ src/main/java/leetcode/stack/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 队列相关题目
+ *
+ *
+ * @author haze
+ * @date created at 2020/2/3 8:40 下午
+ */
+package leetcode.stack;
