61ac8396cdbe5da9612c0b6d85fb3217047bd81f
haxe <792272581@qq.com>
2020-04-26 12:37:03 +0800

    注释

--- src/main/java/leetcode/linkedlist/MergeList.java
+++ src/main/java/leetcode/linkedlist/MergeList.java
@@ -21,18 +21,18 @@ public class MergeList {
         new MergeList().mergeTwoLists(n1, n4).print();
     }
 
-    // 1->2->4
-    // 2->3->6
-
     /**
+     * 1->2->4
+     * 2->3->6
      * 通过递归方式串起两个链表
-     * 1   2  4
-     * | / | /|
-     * 2   3  6
-     * 1.定义函数返回值
+     * 1   2   4
+     * | / | / |
+     * 2   3   6
+     * 1.定义函数返回值:将两个链表有序合并
      */
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
         //2.定义结束条件
+        //2个链表,当其中一个l1到达结束之处(l1=null),将l2返回作为拼接的结果
         if (l1 == null) {
             return l2;
         }
@@ -42,6 +42,11 @@ public class MergeList {
 
         //3.等价关系式
         if (l1.val < l2.val) {
+            //l1.val<l2.val,则将l1的下一个元素与l2比较,决定串联方向
+            //l1.next接上串联完成的有序新链表
+            // Q:为什么不是l2.next接上?
+            // A:l1是l1与l2排序得到的有序头结点,l1.next链表进入新的排序比较
+
             l1.next = mergeTwoLists(l1.next, l2);
             return l1;
         } else {
